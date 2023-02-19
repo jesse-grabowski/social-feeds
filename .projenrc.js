d7112f95053b1bd39954be08ce4f62f490284b4a
Jesse Grabowski <github@jessegrabowski.com>
2023-02-19 09:35:46 -0500

    docs: style documentation and simplify build

--- .projenrc.js
+++ .projenrc.js
@@ -4,6 +4,10 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   defaultReleaseBranch: 'main',
   name: 'social-feeds',
   gitignore: ['.idea', 'docs/dist'],
+  devDeps: ['concurrently', 'onchange', 'http-server'],
+  scripts: {
+    docs: 'concurrently -k "onchange \'docs/**/*.adoc\' -- asciidoctor -r asciidoctor-diagram -D \'./docs/dist\' \'./docs/index.adoc\'" "http-server \'./docs/dist\'"',
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
